dfcd1a2faeee015b6105887e9df9ff2fe11f9b48
himynameismoose <mershellemae@gmail.com>
2022-05-17 10:53:41 -0700

    Initial commit

---
+++ src/main/java/com/himynameismoose/foodtruckwebapp/FoodtruckWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.himynameismoose.foodtruckwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtruckWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtruckWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/himynameismoose/foodtruckwebapp/FoodtruckWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himynameismoose.foodtruckwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodtruckWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
